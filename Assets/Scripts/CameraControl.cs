7f842ebeebbf4290b5e95293736fd90bf615b33a
Sercan Altundaş <sercan.altundas@gmail.com>
2018-01-05 15:43:26 +0200

    Basic scene and player movement.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private float rotationY;
+    private float rotationX;
+
+    public float sensitivity;
+    public Vector2 maxAngles;
+    public Transform cameraPinPoint;
+
+	void Update ()
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensitivity;
+        rotationY -= Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY = Mathf.Clamp(rotationY, maxAngles.x, maxAngles.y);
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(rotationY, rotationX, 0.0f));
+        transform.rotation = targetRotation;
+
+        transform.position = cameraPinPoint.position;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    #region private variables
+    private float verticalAxis;
+    private float horizontalAxis;
+    private float rotationX;
+    #endregion
+
+    #region public variables
+    [Range(0,1)]
+    public float movementSpeed;
+    public float sensitivity;
+    #endregion
+
+    void Update ()
+    {
+        verticalAxis = Input.GetAxis("Vertical");
+        horizontalAxis = Input.GetAxis("Horizontal");
+        rotationX += Input.GetAxis("Mouse X") * sensitivity;
+
+        Vector3 movementDirection = new Vector3(horizontalAxis, 0, verticalAxis);
+        transform.Translate(movementDirection * movementSpeed);
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, rotationX, 0));
+        transform.rotation = targetRotation;
+	}
+}
