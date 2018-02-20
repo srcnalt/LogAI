83e4e6cd6ae241c7349487e90901d91ddd47416a
Sercan Altundaş <sercan.altundas@gmail.com>
2018-02-20 23:05:52 +0200

    Refactor: Logs dir changed, some editor code for logs

--- Assets/Editor/UnitDebugEditor.cs
+++ Assets/Editor/UnitDebugEditor.cs
@@ -2,8 +2,24 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System.IO;
 
 [CustomEditor(typeof(LogManager))]
-public class LogManagerEditor : Editor {
-    //TODO: Fold out for visuals of logger
+public class LogManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LogManager lm = (LogManager)target;
+
+        TextAsset[] test = Resources.LoadAll<TextAsset>("Logs/");
+
+        lm.logs = new List<TextAsset>(test);
+
+        if (GUILayout.Button("Build Object"))
+        {
+            
+        }
+    }
 }
